c1161aa0f6fb8eb34018ff1fcc26c257d7821a37
Quytelda Kahja <quytelda@tamalin.org>
2016-12-26 16:08:25 -0600

    Add highlight theme (based on powerline-default-theme).

--- powerline-themes.el
+++ powerline-themes.el
@@ -85,6 +85,66 @@
 			     (powerline-render rhs)))))))
 
 ;;;###autoload
+(defun powerline-highlight-theme ()
+  "Setup the default mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (hl-face (if active 'powerline-active-hl 'powerline-inactive-hl))
+                          (separator-left (intern (format "powerline-%s-%s"
+							  (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-buffer-id hl-face)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " " hl-face)
+                                     (funcall separator-left hl-face mode-line)
+                                     (when powerline-display-mule-info
+                                       (powerline-raw mode-line-mule-info mode-line 'l))
+                                     (powerline-raw mode-line-modified mode-line)
+                                     (powerline-raw mode-line-remote mode-line)
+                                     (when powerline-display-buffer-size
+                                       (powerline-buffer-size mode-line 'l))
+                                     (powerline-raw " " mode-line)
+                                     (funcall separator-left mode-line face1)
+                                     (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) face2 'l))))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+				     (unless window-system
+				       (powerline-raw (char-to-string #xe0a1) face1 'l))
+				     (powerline-raw "%l" face1 'l)
+				     (powerline-raw ":" face1 'l)
+				     (powerline-raw "%c" face1 'l)
+				     (powerline-raw " " face1)
+				     (funcall separator-right face1 mode-line)
+				     (powerline-raw " " mode-line)
+				     (powerline-raw "%6p" mode-line 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face2 face1)))))
+		     (concat (powerline-render lhs)
+			     (powerline-fill face2 (powerline-width rhs))
+			     (powerline-render rhs)))))))
+
+;;;###autoload
 (defun powerline-center-theme ()
   "Setup a mode-line with major and minor modes centered."
   (interactive)
